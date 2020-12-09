4e36f40d2d8907c68e36beeafeb66c68cce16ec6
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-12-09 16:50:57 +0700

    add new html page

---
+++ dragdrop.js
@@ -0,0 +1,13 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("drag", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("drag");
+    ev.target.appendChild(document.getElementById(data));
+  }
